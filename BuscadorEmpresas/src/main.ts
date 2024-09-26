d9c3a272b11c454dabc25ef0745ca8dc985f6fd3
nikyuher <totalgamenico@gmail.com>
2024-09-26 12:02:06 +0200

    vuetify instalado

--- BuscadorEmpresas/src/main.ts
+++ BuscadorEmpresas/src/main.ts
@@ -6,9 +6,22 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
-
 
 app.mount('#app')
--- BuscadorEmpresas/src/views/Login.vue
+++ BuscadorEmpresas/src/views/Login.vue
@@ -72,7 +72,7 @@
   <style scoped>
   .login-container {
     max-width: 400px;
-    margin: 100px auto;
+    margin:  auto;
     padding: 20px;
     background-color: #f5f5f5;
     border-radius: 8px;
