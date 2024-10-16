b34e2b68583de61627da95efc7aa0e8bc0df1ece
nikyuher <totalgamenico@gmail.com>
2024-10-16 14:55:41 +0200

    avances filtro peticiones

--- BuscadorEmpresas/src/components/Admin/ListPeticiones.vue
+++ BuscadorEmpresas/src/components/Admin/ListPeticiones.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { onMounted, computed, ref } from 'vue'
+
 import { usePeticionesStore } from '@/stores/Peticiones'
 import { useLoginStore } from '@/stores/Login'
+import { useCategoriaStore } from '@/stores/Categoria';
+import { useCiudadStore } from '@/stores/Ciudad';
 
 const peticionesStore = usePeticionesStore()
 const LoginStore = useLoginStore()
+const categoriaStore = useCategoriaStore()
+const ciudadStore = useCiudadStore()
 
-const token = LoginStore.token;
+const categorias = computed(() => categoriaStore.listaCategorias);
+const ciudades = computed(() => ciudadStore.listaCiudades)
+
 const idUsuario = LoginStore.usuario?.idUsuario;
 
 const success = ref(false);
@@ -48,15 +55,24 @@ const rechazarPeticion = async (idPeticion: number) => {
     } catch (err) {
         success.value = false;
         error.value = true;
-
         errorMessage.value = `${err}`
     }
 }
 
-onMounted(() => {
-    peticionesStore.GetAllPeticiones()
-    console.log(peticiones);
+const obtenerNombreCategoria = (idCategoria: number) => {
+    const categoria = categorias.value.find(c => c.idCategoria === idCategoria);
+    return categoria?.nombre || 'Sin categoría';
+}
 
+const obtenerNombreCiudades = (idCiudad: number) => {
+    const ciudad = ciudades.value.find(c => c.idCiudad === idCiudad);
+    return ciudad?.nombre || 'Sin Ciudad';
+}
+
+onMounted(async () => {
+    await categoriaStore.GetAllCategorias();
+    await peticionesStore.GetAllPeticiones();
+    await ciudadStore.GetAllCiudades();
 })
 </script>
 
@@ -70,6 +86,8 @@ onMounted(() => {
                     <th>Nombre Empresa</th>
                     <th>Descripción</th>
                     <th>Dirección</th>
+                    <th>Categoria</th>
+                    <th>Ciudad</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
@@ -79,6 +97,8 @@ onMounted(() => {
                     <td>{{ peticion.nombreEmpresa }}</td>
                     <td>{{ peticion.descripcionEmpresa }}</td>
                     <td>{{ peticion.direccionEmpresa }}</td>
+                    <td> {{ obtenerNombreCategoria(peticion.idCategoriaEmpresa) }}</td>
+                    <td>{{ obtenerNombreCiudades(peticion.idCiudadEmpresa) }}</td>
                     <td>
                         <button class="aceptar" @click="aceptarPeticion(peticion.idPeticion)">Aceptar</button>
                         <button class="denegar" @click="rechazarPeticion(peticion.idPeticion)">Rechazar</button>
